706787b9aaeec3c9f147d38d3d85100351eddd72
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2022-09-07 09:33:07 +0200

    Confirm that location and params hooks are still in sync when routes are not nested

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
         <Link to="/blog/coolblog/1">Cool blog</Link>
         <br />
         <Link to="/blog/randomblog/1">Random blog</Link>
+        <br />
+        <Link to="/notnestedblog/notnestedblog/1">Not nested blog</Link>
+        <br />
+        <Link to="/notnestedblog/another/2">Another Not nested blog</Link>
+        <br />
+        <Link to="/notnestedblog/sluggy/3/whatevs">Not nested blog with slug</Link>
       </div>
 
       <hr />
@@ -25,12 +31,32 @@ function App() {
             </Route>
           </Route>
         </Route>
+        <Route path="notnestedblog" element={<NotNestedRouteBlog />} />
+        <Route path="notnestedblog/:blogName" element={<NotNestedRouteBlog />} />
+        <Route path="notnestedblog/:blogName/:postId" element={<NotNestedRouteBlog />} />
+        <Route path="notnestedblog/:blogName/:postId/:postSlug" element={<NotNestedRouteBlog />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 function Blog() {
+  const {pathname} = useLocation()
+  const {blogName,postId,postSlug} = useParams()
+
+  console.log('pathname', pathname);
+  console.log('blogName', blogName);
+
+  return (
+    <div>
+      <p>This is the blog: {blogName}</p>
+      <p>This post has id: {postId}</p>
+      <p>This post has slug: {postSlug || 'undefined'}</p>
+    </div>
+  )
+}
+
+function NotNestedRouteBlog() {
   const {pathname} = useLocation()
   const {blogName,postId,postSlug} = useParams()
 
